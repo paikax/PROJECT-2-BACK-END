803aa49f4ca5b633aff9f3aee6b562eab942a27e
Nguyen Thanh Phong <phongntgcd210422@fpt.edu.vn>
2024-12-04 15:40:18 +0700

    feat API SCRUM 42 - Request System. role/product/category/brand)

--- src/middleware/roleMiddleware.js
+++ src/middleware/roleMiddleware.js
@@ -8,3 +8,18 @@ const authorizeRoles = (...allowedRole) => {
 }
 
 module.exports = authorizeRoles;
+
+const roles = ["guest", "user", "seller", "admin"];
+
+// Middleware để kiểm tra quyền
+const authorizeRole = (requiredRole) => (req, res, next) => {
+  const userRole = req.user ? req.user.role : "guest";
+
+  if (roles.indexOf(userRole) >= roles.indexOf(requiredRole)) {
+    return next();
+  }
+
+  res.status(403).json({ success: false, message: "Access denied." });
+};
+
+module.exports = authorizeRole;
