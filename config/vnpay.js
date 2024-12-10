d5b90d48dec9b3dfdc5f04bff414eb430c67e621
paikax <paikax2060@gmail.com>
2024-12-10 16:57:40 +0900

    fix(api): trigger re-deployment

--- config/vnpay.js
+++ config/vnpay.js
@@ -3,5 +3,5 @@ module.exports = {
   vnp_TmnCode: process.env.VNP_TMN_CODE || "", // VNPay Terminal Code
   vnp_HashSecret: process.env.VNP_HASH_SECRET || "", // VNPay Hash Secret
   vnp_Url: process.env.VNP_URL || "", // VNPay API URL
-  vnp_ReturnUrl: process.env.VNP_RETURN_URL || "", // VNPay Return URL
+  vnp_ReturnUrl: process.env.VNP_RETURN_URL || "", // VNPay Return URL (front-end)
 };
