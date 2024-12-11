5358ed144e6a0a5cc4f3d9afc2ea32a8f9204ff2
BenZimCO <thieu101kh@gmail.com>
2024-12-11 14:36:11 +0700

    fix(api): update request test 3

--- src/index.js
+++ src/index.js
@@ -3,20 +3,6 @@ const os = require("os");
 const app = require("./app");
 require("dotenv").config({ path: "./../development/.env" });
 
-// Cluster setup to utilize multiple CPU cores
-if (cluster.isMaster) {
-  const numCPUs = os.cpus().length;
-  console.log(`Master cluster setting up ${numCPUs} workers...`);
-
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker) => {
-    console.log(`Worker ${worker.process.pid} died`);
-  });
-} else {
-  app.listen(3000, () => {
-    console.log(`Worker ${process.pid} started on port 3000`);
-  });
-}
+app.listen(3000, () => {
+  console.log(`Worker ${process.pid} started on port 3000`);
+});
