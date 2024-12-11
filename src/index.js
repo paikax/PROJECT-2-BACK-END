276fa66e1f136b1692e32f417a5b4e7f4d57ec88
paikax <paikax2060@gmail.com>
2024-12-11 16:37:58 +0900

    fix(api): merge code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ const os = require("os");
 const app = require("./app");
 require("dotenv").config({ path: "./../development/.env" });
 
-app.listen(3000, () => {
-  console.log(`Worker ${process.pid} started on port 3000`);
-});
+// Cluster setup to utilize multiple CPU cores
+if (cluster.isMaster) {
+  const numCPUs = os.cpus().length;
+  console.log(`Master cluster setting up ${numCPUs} workers...`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  app.listen(3000, () => {
+    console.log(`Worker ${process.pid} started on port 3000`);
+  });
+}
